0816ff900b6f03f3a2a6d7d6c98e74822e5c2e10
Asmit Gaurav Srivastav <Asmit@Etechaces.com>
2017-11-02 21:49:13 +0530

    cross origin

--- server.js
+++ server.js
@@ -7,7 +7,24 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
-
+app.use(function (req, res, next) {
+    
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', 'http://10.80.10.140:80');
+    
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+    
+        // Pass to next layer of middleware
+        next();
+    });
 //var MongoClient = require('mongodb').MongoClient;
 //var url = "mongodb://10.0.8.62:27017/test";
 
